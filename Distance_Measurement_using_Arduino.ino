038da26ec78cf0104b51be508c97fe949738d3dd
amrg101 <76697570+amrg101@users.noreply.github.com>
2021-01-01 14:06:48 +0200

    Update Distance_Measurement_using_Arduino.ino

--- Distance_Measurement_using_Arduino.ino
+++ Distance_Measurement_using_Arduino.ino
@@ -1,17 +1,18 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16,2);
+#include <LiquidCrystal.h>              //بنعمل استيراد للمكتبات ال هنستخدمها
+LiquidCrystal lcd(0LiquidCrystal lcd(7, 8, 9, 10, 11, 12);x27, 16,2);   //بنحدد هنربط الشاشه فين ف الاردوينو
 const int trigPin = 3;
-const int echoPin = 2;
+const int echoPin = 2;                    
 long duration;
-int distanceCm, distanceInch;
+int distanceCm, distanceInch;            // بنعمل تعريف للمتغيرات و بنربط البين بالتريج والايكو
 void setup() {
   lcd.init();
-  lcd.begin(16, 2); // Initializes the interface to the LCD display
-  lcd.backlight();
-  pinMode(trigPin, OUTPUT);
+  lcd.begin(16, 2);                      // Initializes the interface to the LCD display
+  lcd.clear();
+  lcd.backlight();                       //بنصطب ال lcd 
+  pinMode(trigPin, OUTPUT);              // بنحددالمدخلات والمخرجات
   pinMode(echoPin, INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);                   //هنا بنحدد معدل سرعة نقل البيانات 
 }
 void loop() {
   digitalWrite(trigPin, LOW);
@@ -22,9 +23,8 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distanceCm = duration * 0.0340 / 2;
   distanceInch = duration * 0.01330 / 2;
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);                  //بنحدد الطباعه هتبقا فين على الشاشه
   lcd.print("Distance: ");
-  Serial.println(distanceCm);
   lcd.print(distanceCm);
   lcd.print(" cm");
   lcd.setCursor(0, 1);
@@ -32,4 +32,5 @@ void loop() {
   lcd.print(distanceInch);
   lcd.print(" inch");
   delay(1000);
+  lcd.clear();
 }
